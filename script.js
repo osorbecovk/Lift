ad5b4417c5ba962adab405c21b10de6c1e8304fa
osorbecovk <osorbecov@gmail.com>
2024-12-28 20:21:19 +0600

    practica js

---
+++ script.js
@@ -0,0 +1,70 @@
+let btn1 = document.querySelector(".btn1");
+let btn2 = document.querySelector(".btn2");
+let btn3 = document.querySelector(".btn3");
+let btn4 = document.querySelector(".btn4");
+let btn5 = document.querySelector(".btn5");
+let btn6 = document.querySelector(".btn6");
+let btn7 = document.querySelector(".btn7");
+let btn8 = document.querySelector(".btn8");
+let btn9 = document.querySelector(".btn9");
+let btn10 = document.querySelector(".btn10");
+let person = document.querySelector(".person");
+
+person.style.position = "absolute";
+person.style.bottom = "6px"; 
+
+btn1.addEventListener("click", () => {
+    person.style.bottom = "5px";
+    person.style.transition = "1s";
+  });
+
+btn2.addEventListener("click", () => {
+    person.style.bottom = "55px";
+    person.style.transition = "1s";
+});
+
+btn3.addEventListener("click", () => {
+    person.style.bottom = "105px";
+    person.style.transition = "1s";
+});
+
+btn4.addEventListener("click", () => {
+    person.style.bottom = "155px";
+    person.style.transition = "1s";
+});
+
+btn5.addEventListener("click", () => {
+    person.style.bottom = "205px";
+    person.style.transition = "1s";
+});
+
+btn6.addEventListener("click", () => {
+    person.style.bottom = "255px";
+    person.style.transition = "1s";
+});
+
+btn7.addEventListener("click", () => {
+    person.style.bottom = "305px";
+    person.style.transition = "1s";
+});
+
+btn8.addEventListener("click", () => {
+    person.style.bottom = "355px";
+    person.style.transition = "1s";
+});
+
+btn9.addEventListener("click", () => {
+    person.style.bottom = "405px";
+    person.style.transition = "1s";
+});
+
+btn10.addEventListener("click", () => {
+    person.style.bottom = "455px";
+    person.style.transition = "1s";
+  });
+
+
+
+
+  
+
